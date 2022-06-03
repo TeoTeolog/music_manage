638f95d2197e1d78a65e941e94711ae1f077c245
Alek <Alek@exmple.com>
2022-06-03 12:34:54 +0300

    -second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import React, {useEffect, useRef, useState} from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [cnt,setCnt] = useState('')
+    const [cnt1,setCnt1] = useState(-1)
+        useEffect(()=>{
+            setCnt1(cnt1+1)
+        },[cnt])
+      return (
+        <div className="App">
+          <h1>{cnt1}</h1>
+          <input type="text" onChange={(event)=>{setCnt(event.target.value)}} value={cnt}/>
+          <input type="text" onChange={(event)=>{setCnt1(parseInt(event.target.value))}} value={cnt1}/>
+        </div>
+      );
 }
 
 export default App;
